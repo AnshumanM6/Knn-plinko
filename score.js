137f7ea52d8c8a5ca4ddb1fb3b8cf60b75fecefd
AnshumanM6 <43702205+AnshumanM6@users.noreply.github.com>
2019-11-06 22:44:29 +0530

    Add files via upload

---
+++ score.js
@@ -0,0 +1,78 @@
+const output=[]
+
+
+function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
+	output.push([dropPosition, bounciness, size, bucketLabel]);
+}
+
+function runAnalysis() {
+	
+const testsize=100;
+const k=10;
+
+
+
+_.range(0,3).forEach(feature=>{
+const data=_.map(output,row=>[row[feature],_.last(row)]);
+const [testSet,trainingSet]=splitDataset(minMax(data,1),testsize);
+const accuracy=_.chain(testSet)
+.filter(pp=>knn(trainingSet,_.initial(pp),k)===_.last(pp))
+.size()
+.divide(testsize)
+.value();
+
+console.log('for feature of',feature,'Accuracy is',accuracy);
+});
+} 
+
+function knn(data,point,k){
+ return _.chain(data)
+  .map(row=>{
+	  return[
+	    distance(_.initial(row),point),
+		 _.last(row)
+		];
+  })
+  .sortBy(row=>row[0])
+  .slice(0,k)
+  .countBy(row=>row[1])
+  .toPairs()
+  .sortBy(row=>row[1])
+  .last()
+  .first()
+  .parseInt()
+  .value();
+}
+
+function distance(pointA,pointB){	
+return _.chain(pointA)
+.zip(pointB)
+.map(([a,b])=>(a-b)**2)
+.sum()
+.value()**0.5;
+}
+
+function splitDataset(data,testCount){
+	const shuffled=_.shuffle(data);
+	const testSet=_.slice(shuffled,0,testCount);
+	const trainingSet=_.slice(shuffled,testCount);
+	
+	return [testSet,trainingSet];
+}
+
+function minMax(data,featureCount){
+	const clonedData=_.cloneDeep(data);
+	for(let i=0;i<featureCount;i++){
+		const column=clonedData.map(row=>row[i]);
+		const min=_.min(column);
+		const max=_.max(column);
+		
+		for(let j=0;j<clonedData.length;j++){
+			clonedData[j][i]=(clonedData[j][i]-min)/(max-min);
+			
+		}
+	
+	
+}
+return clonedData;
+}
